488ead4b5437b4378987412fcce08ff6e51694db
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2022-01-22 22:11:08 +0100

    Add files via upload

--- yokaiProject/src/Main.java
+++ yokaiProject/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Yokai game = new Yokai(null, null, null);
+        game.play();
     }
 }
